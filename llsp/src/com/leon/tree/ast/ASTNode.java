364119bd27bac1c880e985a84b7e310f1c868494
leonchen83 <529588951@qq.com>
2013-08-19 22:44:17 +0800

    ast tree

---
+++ llsp/src/com/leon/tree/ast/ASTNode.java
@@ -0,0 +1,16 @@
+package com.leon.tree.ast;
+
+
+/** @author : Leon
+ * @since   : 2013-8-19
+ * @see    : 
+ */
+
+public class ASTNode {
+    public ASTNodeType type;
+    public ASTNode syn;
+    public ASTNode inh;
+    public ASTNode node;
+}
+
+
---
+++ llsp/src/com/leon/tree/ast/ASTNodeType.java
@@ -0,0 +1,13 @@
+package com.leon.tree.ast;
+
+
+/** @author : Leon
+ * @since   : 2013-8-19
+ * @see    : 
+ */
+
+public enum ASTNodeType {
+    
+}
+
+
