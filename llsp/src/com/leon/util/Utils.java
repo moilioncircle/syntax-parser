4cb713ba0e40c09fa3483ac12bc33e00c37bfd7f
leonchen83 <529588951@qq.com>
2013-09-14 14:15:47 +0800

    code format

--- llsp/src/com/leon/util/Utils.java
+++ llsp/src/com/leon/util/Utils.java
@@ -3,7 +3,6 @@ package com.leon.util;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
