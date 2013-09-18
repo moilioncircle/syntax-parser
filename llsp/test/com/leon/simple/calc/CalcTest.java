f0c82c6e938b8f991f7c34d42516d4222abafac2
chenbaoyi <529588951@qq.com>
2013-09-18 09:46:21 +0800

    add calc test

---
+++ llsp/test/com/leon/simple/calc/CalcTest.java
@@ -0,0 +1,26 @@
+
+package com.leon.simple.calc;
+
+import java.io.File;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.leon.generator.Compile;
+import com.leon.simple.Token;
+
+/**
+ * @author : Leon
+ * @since : 2013-9-18
+ * @see :
+ */
+
+public class CalcTest {
+    
+    @Test
+    public void testCalc() throws Exception {
+        Compile scg = new Compile();
+        T o = (T) scg.ast(new File("resourses/calc.g"), new File("resourses/example_calc.ca"), Token.class);
+        Assert.assertEquals(7229, o.value());
+    }
+}
